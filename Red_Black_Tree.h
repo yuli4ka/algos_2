1e49d2020f5bcb433874482e01bf9ca10ae4d0e0
yuli4ka <36247684+yuli4ka@users.noreply.github.com>
2018-04-09 17:06:20 +0300

    must add menu

---
+++ Red_Black_Tree.h
@@ -0,0 +1,480 @@
+#ifndef RED_BLACK_TREE_H_INCLUDED
+#define RED_BLACK_TREE_H_INCLUDED
+#pragma once
+
+namespace spaceRed_Black_Tree{
+	template<class T>
+	class Red_Black_Tree{
+		struct TreeNode{
+			T data;
+			bool red;
+			size_t size;
+			TreeNode *left;
+			TreeNode *right;
+			TreeNode *parent;
+			TreeNode() = default;
+			TreeNode(T _data, TreeNode * _parent, TreeNode *nil) : data(_data), red(true), size(1), parent(_parent), left(nil), right(nil) {}
+		};
+
+		TreeNode *main_root;
+		TreeNode *nil;
+	public:
+		Red_Black_Tree(){
+			nil = new TreeNode();
+			nil->red = false;
+			nil->left = nil;
+			nil->right = nil;
+			nil->parent = nil;
+			nil->size = 0;
+			this->main_root = nil;
+		}
+
+		Red_Black_Tree(const Red_Black_Tree &obj){
+			nil = new TreeNode();
+			nil->red = false;
+			nil->left = nil;
+			nil->right = nil;
+			nil->parent = nil;
+			nil->size = 0;
+			if (obj.main_root == obj.nil)
+				this->main_root = this->nil;
+			else{
+				this->main_root = new TreeNode;
+				this->main_root->parent = this->nil;
+				copy(this->main_root, obj.main_root, obj.nil);
+			}
+		}
+		~Red_Black_Tree(){
+			clear(this->main_root);
+			delete nil;
+			this->main_root = nullptr;
+			this->nil = nullptr;
+		}
+
+		Red_Black_Tree& operator=(const Red_Black_Tree &obj){
+			clear(this->main_root);
+			if (obj.main_root == obj.nil)
+				this->main_root = this->nil;
+			else{
+				this->main_root = new TreeNode;
+				this->main_root->parent = this->nil;
+				copy(this->main_root, obj.main_root, obj.nil);
+			}
+			return *this;
+		}
+
+		bool Insert(T data){
+			if (this->main_root == nil){
+				this->main_root = new TreeNode(data, nil, nil);
+				this->main_root->red = false;
+				return false;
+			}
+			TreeNode *p = this->main_root, *current = nullptr;
+			while (p != nil){
+				current = p;
+				if (p->data < data) p = p->right;
+				else if (p->data > data) p = p->left;
+				else return true;
+			}
+			/////
+			p = current;
+			while (p != nil){
+				p->size++;
+				p = p->parent;
+			}
+			//////
+			if (current->data < data){
+				current->right = new TreeNode(data, current, nil);
+				insertBalance(current->right);
+			}
+			else{
+				current->left = new TreeNode(data, current, nil);
+				insertBalance(current->left);
+			}
+			while (current != nil){
+				insertBalance(current);
+				current = current->parent;
+			}
+			return false;
+		}
+
+		bool Delete(T data){
+			TreeNode *current = this->main_root;
+			while (current->data != data && current != nil){
+				if (current->data > data)
+                    current = current->left;
+				else if (current->data < data)
+                    current = current->right;
+			}
+			if (current == nil)
+                return false;
+			TreeNode *sub = current;
+			if (current->left != nil){
+				sub = current->left;
+				while (sub->right != nil)
+                    sub = sub->right;
+			}
+			else if (current->right != nil){
+				sub = current->right;
+				while (sub->left != nil)
+                    sub = sub->left;
+			}
+			current->data = sub->data;
+			if (sub == this->main_root){
+				delete this->main_root;
+				this->main_root = nil;
+				return true;
+			}
+			////
+			current = sub;
+			while (current != nil){
+				current->size--;
+				current = current->parent;
+			}
+			//////
+			if (sub->left == nil && sub->right == nil){
+				if (sub->red){
+					if (sub->parent->left == sub)
+                        sub->parent->left = nil;
+					else
+                        sub->parent->right = nil;
+					delete sub;
+				}
+				else{
+					deleteBalance(sub);
+					if (sub->parent->left == sub)
+                        sub->parent->left = nil;
+					else
+                        sub->parent->right = nil;
+					delete sub;
+				}
+			}
+			else{
+				if (sub->left != nil){
+					sub->data = sub->left->data;
+					delete sub->left;
+					sub->left = nil;
+				}
+				else{
+					sub->data = sub->right->data;
+					delete sub->right;
+					sub->right = nil;
+				}
+			}
+			return true;
+		}
+
+	private:
+		void clear(TreeNode *node){
+			if (node == nil)
+                return;
+			clear(node->left);
+			clear(node->right);
+			delete node;
+		}
+		void copy(TreeNode *into, TreeNode *from, TreeNode *nil_from){
+			if (from == nil_from){
+				*into = *nil;
+				return;
+			}
+			into->data = from->data;
+			into->size = from->size;
+			into->red = from->red;
+
+			into->left = new TreeNode;
+			into->left->parent = into;
+			into->right = new TreeNode;
+			into->right->parent = into;
+
+			copy(into->left, from->left, nil_from);
+			copy(into->right, from->right, nil_from);
+		}
+
+		inline void insertBalance(TreeNode *current){
+			if (!current->red || !current->parent->red) return;
+			TreeNode *grandparent = current->parent->parent;
+			TreeNode *uncle = (grandparent->left == current->parent ? grandparent->right : grandparent->left);
+			if (uncle->red){
+				grandparent->red = true;
+				uncle->red = false;
+				current->parent->red = false;
+			}
+			else{
+				grandparent->red = true;
+				if (current->parent->left == current){
+					if (grandparent->left == uncle){
+                        current->red = false;
+                        RL_rotate(grandparent);
+                    }
+					else{
+                        current->parent->red = false;
+                        R_rotate(grandparent);
+                    }
+				}
+				else{
+					if (grandparent->left == uncle){
+                        current->parent->red = false;
+                        L_rotate(grandparent);
+                    }
+					else{
+					    current->red = false;
+                        LR_rotate(grandparent);
+                    }
+				}
+			}
+			this->main_root->red = false;
+		}
+
+		inline void deleteBalance(TreeNode *current){
+			while (current != this->main_root){
+				TreeNode *parent = current->parent;
+				TreeNode *bro = (parent->left == current ? parent->right : parent->left);
+				if (parent->red){
+					if (bro->left->red || bro->right->red){
+						parent->red = false;
+						if (parent->left == current){
+							if (bro->left->red) RL_rotate(parent);
+							else{
+                                bro->red = true;
+                                bro->right->red = false;
+                                L_rotate(parent);
+                            }
+						}
+						else{
+							if (bro->left->red){
+                                bro->red = true;
+                                bro->left->red = false;
+                                R_rotate(parent);
+                            }
+							else
+                                LR_rotate(parent);
+						}
+						return;
+					}
+					else{
+						bro->red = true;
+						parent->red = false;
+						return;
+					}
+				}
+				else{
+					if (bro->red){
+						if (bro->left->left->red || bro->left->right->red || bro->right->left->red || bro->right->right->red){
+							if (bro->parent->left == bro){
+								if (bro->left->left->red || bro->left->right->red){
+                                    bro->red = false;
+                                    bro->right->red = true;
+                                    R_rotate(parent);
+                                }
+								else if (bro->right->left->red){
+                                    bro->right->left->red = false;
+                                    LR_rotate(parent);
+                                }
+								else if (bro->right->right->red){
+                                    bro->right->right->red = false;
+                                    L_rotate(bro->right);
+                                    LR_rotate(parent);
+                                }
+							}
+							else{
+								if (bro->left->left->red){
+                                    bro->left->left->red = false;
+                                    R_rotate(bro->left);
+                                    RL_rotate(parent);
+                                }
+								else if (bro->left->right->red){
+                                    bro->left->right->red = false;
+                                    RL_rotate(parent);
+                                }
+								else if (bro->right->left->red || bro->right->right->red){
+                                    bro->red = false;
+                                    bro->left->red = true;
+                                    L_rotate(parent);
+                                }
+							}
+							return;
+						}
+						else{                           // 2.1.2
+							bro->red = false;
+							if (bro == bro->parent->left){
+								bro->right->red = true;
+								R_rotate(parent);
+							}
+							else{
+								bro->left->red = true;
+								L_rotate(parent);
+							}
+							return;
+						}
+					}
+					else{
+						if (bro->left->red || bro->right->red){                         // 2.2.1
+							if (parent->left == current){
+								if (bro->left->red){
+                                    bro->left->red = false;
+                                    RL_rotate(parent);
+                                }
+								else{
+                                    bro->right->red = false;
+                                    L_rotate(parent);
+                                }
+							}
+							else{
+								if (bro->left->red){
+								    bro->left->red = false;
+                                    R_rotate(parent);
+                                }
+								else{
+                                    bro->right->red = false;
+                                    LR_rotate(parent);
+                                }
+							}
+							return;
+						}
+						else{                       // 2.2.2
+							bro->red = true;
+							current = current->parent;
+						}
+					}
+				}
+			}
+		}
+		//
+		inline void L_rotate(TreeNode *p1){
+			TreeNode *p2 = p1->right;
+			p1->right = p2->left;
+			if (p2->left != nil)
+                p2->left->parent = p1;
+			p2->parent = p1->parent;
+			if (p1->parent != nil){
+				if (p1->parent->left == p1)
+                    p1->parent->left = p2;
+				else
+                    p1->parent->right = p2;
+			}
+			p1->parent = p2;
+			p2->left = p1;
+			if (p1 == this->main_root)
+                this->main_root = p2;
+			p2->size = p1->size;
+			p1->size = p1->left->size + p1->right->size + 1;
+		}
+
+		inline void R_rotate(TreeNode *p1){
+			TreeNode *p2 = p1->left;
+			p1->left = p2->right;
+			if (p2->right != nil)
+                p2->right->parent = p1;
+			p2->parent = p1->parent;
+			if (p1->parent != nil){
+				if (p1->parent->left == p1)
+                    p1->parent->left = p2;
+				else
+                    p1->parent->right = p2;
+			}
+			p1->parent = p2;
+			p2->right = p1;
+			if (p1 == this->main_root)
+                this->main_root = p2;
+			p2->size = p1->size;
+			p1->size = p1->left->size + p1->right->size + 1;
+		}
+
+		inline void LR_rotate(TreeNode *p1){
+			TreeNode *p2 = p1->left;
+			TreeNode *p3 = p2->right;
+			p2->right = p3->left;
+			if (p3->left != nil)
+                p3->left->parent = p2;
+			p3->left = p2;
+			p3->parent = p1->parent;
+			p2->parent = p3;
+			if (p1->parent != nil){
+				if (p1->parent->left == p1)
+                    p1->parent->left = p3;
+				else
+                    p1->parent->right = p3;
+			}
+			p1->left = p3->right;
+			if (p3->right != nil)
+                p3->right->parent = p1;
+			p3->right = p1;
+			p1->parent = p3;
+			if (this->main_root == p1)
+                this->main_root = p3;
+
+			p3->size = p1->size;
+			p1->size = p1->left->size + p1->right->size + 1;
+			p2->size = p2->left->size + p2->right->size + 1;
+		}
+
+		inline void RL_rotate(TreeNode *p1){
+			TreeNode *p2 = p1->right;
+			TreeNode *p3 = p2->left;
+			p2->left = p3->right;
+			if (p3->right != nil)
+                p3->right->parent = p2;
+			p3->right = p2;
+			p3->parent = p1->parent;
+			p2->parent = p3;
+			if (p1->parent != nil){
+				if (p1->parent->left == p1)
+                    p1->parent->left = p3;
+				else
+                    p1->parent->right = p3;
+			}
+			p1->right = p3->left;
+			if (p3->left != nil)
+                p3->left->parent = p1;
+			p3->left = p1;
+			p1->parent = p3;
+			if (this->main_root == p1)
+                this->main_root = p3;
+
+			p3->size = p1->size;
+			p1->size = p1->left->size + p1->right->size + 1;
+			p2->size = p2->left->size + p2->right->size + 1;
+		}
+
+		//
+	public:
+		T FindByIndex(size_t ind){
+			TreeNode *p = this->main_root;
+			while (p && getRank(p) != ind){
+				if (getRank(p->left) < ind)
+                    p = p->right;
+				else if (getRank(p->left) > ind)
+                    p = p->left;
+			}
+			if (!p)
+                return T();
+			return p->data;
+		}
+		size_t GetRank(T data){
+			TreeNode *p = this->main_root;
+			while (p != nil){
+				if (p->data < data)
+                    p = p->right;
+				else if (p->data > data)
+                    p = p->left;
+				else
+                    return getRank(p);
+			}
+			return 0;
+		}
+
+	private:
+		size_t getRank(TreeNode *node){
+			size_t res = node->left->size + 1;
+			while (node != this->nil){
+				if (node->parent->right == node)
+                    res += node->parent->left->size + 1;
+				node = node->parent;
+			}
+			return res;
+		}
+	};
+}
+
+
+#endif // RED_BLACK_TREE_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
-#include <queue>
-#include <string>
-#include <conio.h>
-#include <fstream>
 #include <stdlib.h>
+#include <vector>
+#include "Red_Black_Tree.h"
 using namespace std;
 
-//hh:mm dd.mm.yyyy
 struct date{
     string dat;
     date(){
@@ -19,9 +16,9 @@ struct event{
     string place;
     date datepoint;
     event(){
-        name = " ";
-        place = " ";
-        datepoint.dat = " ";
+        name = "";
+        place = "";
+        datepoint.dat = "";
     }
 };
 
@@ -29,16 +26,19 @@ struct notebook{
     date date;
     vector<event> evets;
     notebook(){
-        date.dat = " ";
-    }
-public:
-    void n_new(string d_new){
-        date.dat = d_new;
+        date.dat = "";
     }
 };
 
 bool operator > (notebook a, notebook b){
     if (a.date.dat > b.date.dat)
+        return true;
+    else
+        return false;
+}
+
+bool operator < (notebook a, notebook b){
+    if (a.date.dat < b.date.dat)
         return true;
     else
         return false;
@@ -51,478 +51,39 @@ bool operator != (notebook a, notebook b){
         return false;
 }
 
-class SizeRedBlackTree
-{
-public:
-	enum nodeColor {RED, BLACK};
-	struct Node
-	{
-		//int data;;
-		notebook data;
-		Node *parent;
-		Node *right;
-		Node *left;
-		nodeColor color;
-		int size;
-	};
-	SizeRedBlackTree()
-	{
-		NIL = new Node;
-		NIL->right = NIL;
-		NIL->left = NIL;
-		NIL->color = BLACK;
-		NIL->parent = NULL;
-		NIL->size = 0;
-		root = NIL;
-
-	}
-	void insertNode(notebook);
-	void insertFixup(Node *);
-	void deleteNode(notebook);
-	void deleteTransplant(Node *, Node *);
-	void deleteFixup(Node *);
-	void leftRotate(Node *);
-	void rightRotate(Node *);
-	Node *select(Node *, int);
-	void print(Node *, int);
-	//void reading();
-	Node *root;
-	Node *NIL;
-	Node *Nod = NIL;
-private:
-
-
-};
-
-void SizeRedBlackTree::insertNode(notebook data){
-	Node *curr = root;
-	Node *parent = NULL;
-	while (curr != NIL){
-		curr->size++;
-		parent = curr;
-		if (data > curr->data)
-			curr = curr->right;
-		else
-			curr = curr->left;
-	}
-	Node *node = new Node;
-	node->data = data;
-	node->left = NIL;
-	node->right = NIL;
-	node->color = RED;
-	node->size = 1;
-	node->parent = parent;
-	if (parent){
-		if (data > parent->data)
-			parent->right = node;
-		else
-			parent->left = node;
-	}
-	else
-		root = node;
-		insertFixup(node);
-}
-
-void SizeRedBlackTree::insertFixup(Node *node){
-	while (node != root && node->parent->color == RED){
-		if (node->parent == node->parent->parent->left){
-			Node *uncle = node->parent->parent->right;
-			if (uncle->color == RED){
-				node->parent->color = BLACK;
-				uncle->color = BLACK;
-				node->parent->parent->color = RED;
-				node = node->parent->parent;
-			}
-			else{
-				if (node = node->parent->right){
-					node = node->parent;
-					leftRotate(node);
-				}
-				node->parent->color = BLACK;
-				node->parent->parent->color = RED;
-				rightRotate(node->parent->parent);
-			}
-		}
-		else{
-			Node *uncle = node->parent->parent->left;
-			if (uncle->color == RED){
-				node->parent->color = BLACK;
-				uncle->color = BLACK;
-				node->parent->parent->color = RED;
-				node = node->parent->parent;
-			}
-			else{
-				if (node = node->parent->right){
-					node = node->parent;
-					rightRotate(node);
-				}
-				node->parent->color = BLACK;
-				node->parent->parent->color = RED;
-				leftRotate(node->parent->parent);
-			}
-		}
-	}
-	root->color = BLACK;
-}
-
-void SizeRedBlackTree::leftRotate(Node *node){
-	Node *pilot = node->right;
-	node->right = pilot->left;
-	if (pilot->left != NIL)
-		pilot->left->parent = node;
-	pilot->parent = node->parent;
-	if (node->parent){
-		if (node == node->parent->left)
-			node->parent->left = pilot;
-		else
-			node->parent->right = pilot;
-	}
-	else
-		root = pilot;
-	pilot->left = node;
-	node->parent = pilot;
-	pilot->size = node->size;
-	node->size = node->left->size + node->right->size + 1;
-}
-
-void SizeRedBlackTree::rightRotate(Node *node){
-	Node *pilot = node->left;
-	node->left = pilot->right;
-	if (pilot->right != NIL)
-		pilot->right->parent = node;
-	pilot->parent = node->parent;
-	if (node->parent){
-		if (node == node->parent->right)
-			node->parent->right = pilot;
-		else
-			node->parent->left = pilot;
-	}
-	else
-		root = pilot;
-	pilot->right = node;
-	node->parent = pilot;
-	pilot->size = node->size;
-	node->size = node->left->size + node->right->size + 1;
-}
-
-
-void SizeRedBlackTree::deleteTransplant(Node *u, Node *v){
-	if (!u->parent)
-		root = v;
-	else if (u == u->parent->left)
-		u->parent->left = v;
-	else
-		u->parent->right = v;
-	u->parent = v->parent;
-}
-
-void SizeRedBlackTree::deleteNode(notebook data){
-	Node *curr = root;
-	Node *parent = NULL;
-	while (curr->data != data){
-		curr->size--;
-		if (data > curr->data)
-			curr = curr->right;
-		else
-			curr = curr->left;
-	}
-	Node *fix;
-	nodeColor color = curr->color;
-	if (curr->left == NIL && curr->right == NIL){
-		if (curr->parent)
-			fix = curr->parent;
-		else{
-			root = NIL;
-			delete curr;
-			return;
-		}
-		if (curr == curr->parent->left)
-			curr->parent->left = NIL;
-		else
-			curr->parent->right = NIL;
-	}
-	else if (curr->left != NIL && curr->right != NIL){
-		Node *toDel = curr->right;
-		while (toDel->left != NIL){
-			toDel->size--;
-			toDel = toDel->left;
-		}
-		color = toDel->color;
-		fix = toDel->right;
-		/////
-		if (toDel->parent != curr){
-			deleteTransplant(toDel, toDel->right);
-			toDel->right = curr->right;
-			toDel->right->parent = toDel;
-		}
-		deleteTransplant(curr, toDel);
-		toDel->left = curr->left;
-		toDel->left->parent = toDel;
-		toDel->color = curr->color;
-	}
-	else if(curr->left == NIL){
-		fix = curr->right;
-		deleteTransplant(curr, curr->right);
-	}
-	else{
-		fix = curr->left;
-		deleteTransplant(curr, curr->left);
-	}
-	if (color == BLACK)
-		deleteFixup(fix);
-	delete curr;
-}
-
-void SizeRedBlackTree::deleteFixup(Node *node){
-	Node *uncle;
-	while (node != root && node->color == BLACK){
-		if (node = node->parent->left){
-			uncle = node->parent->right;
-			if (uncle->color == RED){
-				uncle->color = BLACK;
-				node->parent->color = RED;
-				leftRotate(node->parent);
-				uncle = node->parent->right;
-			}
-			if (uncle->left->color == BLACK && uncle->right->color == BLACK){
-				uncle->color = RED;
-				node = node->parent;
-			}
-			else if (uncle->right->color == BLACK){
-				uncle->left->color = BLACK;
-				uncle->color = RED;
-				rightRotate(uncle);
-				uncle = node->parent->right;
-			}
-			uncle->color = node->parent->color;
-			node->parent->color = BLACK;
-			uncle->right->color = BLACK;
-			leftRotate(node->parent);
-			node = root;
-		}
-		else{
-			uncle = node->parent->left;
-			if (uncle->color == RED){
-				uncle->color = BLACK;
-				node->parent->color = RED;
-				rightRotate(node->parent);
-				uncle = node->parent->left;
-			}
-			if (uncle->left->color == BLACK && uncle->right->color == BLACK){
-				uncle->color = RED;
-				node = node->parent;
-			}
-			else if (uncle->left->color == BLACK){
-				uncle->right->color = BLACK;//left?
-				uncle->color = RED;
-				leftRotate(uncle);
-				uncle = node->parent->left;
-			}
-			uncle->color = node->parent->color;
-			node->parent->color = BLACK;
-			uncle->left->color = BLACK;
-			rightRotate(node->parent);
-			node = root;
-		}
-	}
-	node->color = BLACK;
-}
-
-/*void SizeRedBlackTree::print(Node *curr){
-    if (curr == NIL){
-        cout << "NIL\t";
-        return;
-    }
-    cout <<curr->data.date.dat << "(";
-    if (curr->color == RED)
-        cout << "RED,";
+bool operator == (notebook a, notebook b){
+    if (a.date.dat == b.date.dat)
+        return true;
     else
-        cout << "BLACK,";
-    cout << curr->size << ")\t";
-    cout << endl;
-    print(curr->left);
-    print(curr->right);
-    ///
-}*/
-
-SizeRedBlackTree::Node *SizeRedBlackTree::select(Node *node, int dat){
-	int t_size = node->left->size + 1;
-	if (node == NIL)
-        return NIL;
-	if (dat == t_size)
-		return node;
-	else if (dat < t_size)
-		return select(node->left, dat);
-	else
-		return select(node->right, dat - t_size);
-}
-
-/*void SizeRedBlackTree::reading(){ //problems
-    ifstream input("database.txt");
-    if (!input.is_open()){
-        cout << "Oooops. Batabase is clear(cannot find file)" << endl;
-        system("Pause");
-        return;
-    }
-    string data = "";
-    while ((!input.eof()) && getline(input,data)){
-        notebook new_notebook;
-        new_notebook.date.dat = data;
-        cout << data << endl;
-        while ((!input.eof()) && getline(input,data)){
-            if (data == "//")
-                break;
-            event new_event;
-            new_event.name = data;
-            getline(input,data);
-            new_event.place = data;
-            new_event.datepoint.dat = new_notebook.date.dat;
-            new_notebook.evets.push_back(new_event);
-        }
-        insertNode(new_notebook);
-    }
-    input.close();
-}*/
-
-void reading(vector<notebook> &v_notebook, vector<event> &v_event){
-    ifstream input("database.txt");
-    if (!input.is_open()){
-        cout << "Oooops. Batabase is clear(cannot find file)" << endl;
-        system("Pause");
-        return;
-    }
-    string data = "";
-    while ((!input.eof()) && getline(input,data)){
-        notebook new_notebook;
-        new_notebook.date.dat = data;
-
-        while ((!input.eof()) && getline(input,data)){
-            if (data == "//")
-                break;
-            event new_event;
-            new_event.name = data;
-            getline(input,data);
-            new_event.place = data;
-            new_event.datepoint.dat = new_notebook.date.dat;
-            new_notebook.evets.push_back(new_event);
-            v_event.push_back(new_event);
-        }
-        v_notebook.push_back(new_notebook);
-    }
-    input.close();
-}
-
-void SizeRedBlackTree::print(Node *p, int level){
-    if(p != NULL){
-        if (p->right != NULL && p->right != NIL)
-            print(p->right, level+1);
-        for(int i = 0; i < level; i++) cout<<"\t";
-        if (p != NULL && p!= NIL)
-            cout<< p->data.date.dat <<endl;
-        else
-            cout<<"NIL"<<endl;
-        if (p->left != NULL && p->left != NIL)
-            print(p->left, level+1);
-    }
-    //if (p == NIL)
-      //  cout <<"NIL"<<endl;
-}
-
-int menu() {
-        char key=0;
-        int code;
-        do {
-                system("cls");
-                key=(key+3)%3;
-                if (key==0) cout<<"-> Search data."<<endl;
-                        else  cout<<"   Search data."<<endl;
-                if (key==1) cout<<"-> Print tree."<<endl;
-                        else  cout<<"   Print tree."<<endl;
-                if (key==2) cout<<"-> Exit."<<endl;
-                        else  cout<<"   Exit."<<endl;
-                code=_getch();
-                if (code==224)
-                {
-                    code=_getch();
-                    if (code==80) key++;
-                    if (code==72) key--;
-                        }
-           }while(code!=13);
-        system("cls");
-        return key;
+        return false;
 }
 
 int main()
 {
-	SizeRedBlackTree rbt;
-	vector<notebook> v_notebook;
-    vector<event> v_event;
-    reading(v_notebook, v_event);
-    for (int i = 0; i < v_notebook.size(); i++){
-        cout << v_notebook[i].date.dat << endl;
-        rbt.insertNode(v_notebook[i]);
-    }
-    system("Pause");
-    //rbt.reading();
-    cout << 'x';
-    rbt.print(rbt.root,0);
-    cout << 'x';
-    char c=0;
-    while (c!=4)
-    {
-        c=menu();
-        system("cls");
-        switch (c)
-        {
-            case 0: {
-                int data;
-                cout << "Give me key" << endl;
-                cin >> data;
-                rbt.Nod= rbt.select(rbt.root,data);
-                if (rbt.Nod == rbt.NIL)
-                    cout << "You lose" << endl;
-                else{
-                cout << rbt.Nod->data.date.dat << endl;
-                if (!rbt.Nod->data.evets.size())
-                    cout << "There is no event" << endl;
-                else{
-                    for (long i = 0; i < rbt.Nod->data.evets.size(); i++)
-                        cout << rbt.Nod->data.evets[i].name << " in " << rbt.Nod->data.evets[i].place << endl;
-                }
-                }
-                system("pause");
-                break;
-            }
-            case 1: {
-                rbt.print(rbt.root,0);
-            }
-            case 2: {
-                return 0;
-            }
-            system("cls");
-        }
+	spaceRed_Black_Tree::Red_Black_Tree<notebook> tree;
+	notebook data1;
+	notebook data2;
+	notebook data3;
+	notebook data4;
 
-    }
-	/*notebook note;
-	note.n_new("5");
-	rbt.insertNode(note);
-	note.n_new("2");
-	rbt.insertNode(note);
-	note.n_new("3");
-	rbt.insertNode(note);
-	note.n_new("7");
-	rbt.insertNode(note);
-	note.n_new("4");
-	rbt.insertNode(note);
-	note.n_new("9");
-	rbt.insertNode(note);
-	note.n_new("0");
-	rbt.insertNode(note);
-	note.n_new("6");
-	rbt.insertNode(note);
-	note.n_new("2");
-	rbt.deleteNode(note);
-	rbt.print();*/
+	tree.Insert(data1);
+	tree.Insert(data2);
+	tree.Insert(data3);
+	tree.Insert(data4);
+	/*tree.Insert(2);
+	tree.Insert(3);
+	tree.Insert(4);
+	tree.Insert(5);
+	tree.Insert(6);
+	tree.Insert(7);
 
+	tree.Delete(4);
+
+	for (size_t i = 1; i < 8; i++)
+	{
+		cout << tree.GetRank(i) << endl;
+	}
+*/
+	system("pause");
 	return 0;
 }
