bd5e18f59ea9867a0aa3dae426d6acb09e1ab7e3
yuli4ka <36247684+yuli4ka@users.noreply.github.com>
2018-04-10 10:02:42 +0300

    added overloading. should working with menu

--- main.cpp
+++ main.cpp
@@ -58,18 +58,60 @@ bool operator == (notebook a, notebook b){
     else
         return false;
 }
+
+ostream& operator<<(ostream& os, const notebook& dt)
+{
+    os << dt.date.dat;
+    return os;
+}
+
+ostream& operator<<(ostream& os, const event& dt)
+{
+    os << dt.name << " in " << dt.place << ", " << dt.datepoint.dat;
+    return os;
+}
+
+istream& operator>>(istream& is, notebook& dt)
+{
+    getline(is,dt.date.dat);
+    string temp;
+    getline(is,temp);
+    while (temp != "//"){
+        event new_event;
+        new_event.name = temp;
+        getline(is,temp);
+        new_event.place = temp;
+        dt.evets.push_back(new_event);
+        new_event.datepoint = dt.date;
+        getline(is,temp);
+    }
+    return is;
+}
+
 //
 int menu() {
         char key=0;
         int code;
         do {
                 system("cls");
-                key=(key+3)%3;
-                if (key==0) cout<<"-> Search data."<<endl;
-                        else  cout<<"   Search data."<<endl;
-                if (key==1) cout<<"-> Print tree."<<endl;
-                        else  cout<<"   Print tree."<<endl;
-                if (key==2) cout<<"-> Exit."<<endl;
+                key=(key+9)%9;
+                if (key==0) cout<<"-> Insert date."<<endl;
+                        else  cout<<"   Insert date."<<endl;
+                if (key==1) cout<<"-> Insert event."<<endl;
+                        else  cout<<"   Insert event."<<endl;
+                if (key==2) cout<<"-> Delete date."<<endl;
+                        else  cout<<"   Delete date."<<endl;
+                if (key==3) cout<<"-> Delete event."<<endl;
+                        else  cout<<"   Delete event."<<endl;
+                if (key==4) cout<<"-> Get Rank date."<<endl;
+                        else  cout<<"   Get Rank date."<<endl;
+                if (key==5) cout<<"-> Get Rank event."<<endl;
+                        else  cout<<"   Get Rank event."<<endl;
+                if (key==6) cout<<"-> Print tree date."<<endl;
+                        else  cout<<"   Print tree date."<<endl;
+                if (key==7) cout<<"-> Print tree event."<<endl;
+                        else  cout<<"   Print tree event."<<endl;
+                if (key==8) cout<<"-> Exit."<<endl;
                         else  cout<<"   Exit."<<endl;
                 code=_getch();
                 if (code==224)
@@ -85,11 +127,50 @@ int menu() {
 //
 int main()
 {
+    char c=0;
+    while (c!=9)
+    {
+        c=menu();
+        system("cls");
+        switch (c)
+        {
+            case 0: {
+                notebook data;
+                cout << "Give me date and events (1st word == event, 2nd word == place of event, '//' == end" << endl;
+                cin >> data;
+
+                cout << data << endl;
+                if (data.evets.empty())
+                    cout << "There is not event" << endl;
+                else
+                    cout << data.evets[0] << endl;
+
+                system("pause");
+                break;
+            }
+            case 1: {
+                //rbt.print(rbt.root,0);
+            }
+            case 8: {
+                return 0;
+            }
+            system("cls");
+        }
+
+    }
 	spaceRed_Black_Tree::Red_Black_Tree<notebook> tree;
 	notebook data1;
+	data1.date.dat = "sfd";
+	cout << data1 << endl;
 	notebook data2;
 	notebook data3;
 	notebook data4;
+
+	event dat1;
+	dat1.name = "zz";
+	dat1.place = "cc";
+	dat1.datepoint = data1.date;
+	cout << dat1 << endl;
 
 	tree.Insert(data1);
 	tree.Insert(data2);
